52a876b1e041ba69b962c171aae05c9a465b3ccd
pedro.pereira <pedro.pereira@consuti.com.br>
2022-03-15 12:15:55 -0300

    ajustando eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,54 @@
-import { useState } from 'react'
-import './app.css'
-import Logo05 from './assets/Logo05.png'
+import './app.css';
+import Logo05 from './assets/Logo05.png';
 
-export function App() {
-
-  return ( 
+const App = (): any => {
+  return (
     <div>
-       <div className="App">
-      <header className="App-header">
-        <img src={Logo05} className="App-logo" alt="logo" />
-        <h1>Escutem meu podcast:</h1>
-        <p>
-          <a target='_blank' href="https://open.spotify.com/show/7L3y1F8Nc8JWjQ5ktJjPvA">
-            <button className='button' type="button">
-            Spotify
-            </button>
-          </a>
-          <a target='_blank' href="https://www.deezer.com/br/show/2671052A">
-            <button className='button' type="button">
-            Deezer
-            </button>
-          </a>
-          <a target='_blank' href="https://discord.com/invite/sYy53UnVhaA">
-            <button className='button' type="button">
-            Discord
-            </button>
-          </a>
-          <a target='_blank' href="https://www.instagram.com/fliperamanerdoficial/">
-            <button className='button' type="button">
-            Instagram
-            </button>
-          </a>      
-         </p>
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <img src={Logo05} className="App-logo" alt="logo" />
+          <h1>Escutem meu podcast:</h1>
+          <p>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://open.spotify.com/show/7L3y1F8Nc8JWjQ5ktJjPvA"
+            >
+              <button className="button" type="button">
+                Spotify
+              </button>
+            </a>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.deezer.com/br/show/2671052A"
+            >
+              <button className="button" type="button">
+                Deezer
+              </button>
+            </a>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://discord.com/invite/sYy53UnVhaA"
+            >
+              <button className="button" type="button">
+                Discord
+              </button>
+            </a>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.instagram.com/fliperamanerdoficial/"
+            >
+              <button className="button" type="button">
+                Instagram
+              </button>
+            </a>
+          </p>
+        </header>
+      </div>
     </div>
-    </div>
-    )
-
-  
-
-}
-
-
+  );
+};
+export default App;
