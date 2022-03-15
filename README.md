f847044d2cdac9243a25924aeade18597b7174ba
pedro.pereira <pedro.pereira@consuti.com.br>
2022-03-15 11:45:13 -0300

    template basico

---
+++ README.md
@@ -0,0 +1,1 @@
+## This is a basic template react-vite-typescript with Prettier and EsLint configured.
---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './app.css'
+import Logo05 from './assets/Logo05.png'
+
+export function App() {
+
+  return ( 
+    <div>
+       <div className="App">
+      <header className="App-header">
+        <img src={Logo05} className="App-logo" alt="logo" />
+        <h1>Escutem meu podcast:</h1>
+        <p>
+          <a target='_blank' href="https://open.spotify.com/show/7L3y1F8Nc8JWjQ5ktJjPvA">
+            <button className='button' type="button">
+            Spotify
+            </button>
+          </a>
+          <a target='_blank' href="https://www.deezer.com/br/show/2671052A">
+            <button className='button' type="button">
+            Deezer
+            </button>
+          </a>
+          <a target='_blank' href="https://discord.com/invite/sYy53UnVhaA">
+            <button className='button' type="button">
+            Discord
+            </button>
+          </a>
+          <a target='_blank' href="https://www.instagram.com/fliperamanerdoficial/">
+            <button className='button' type="button">
+            Instagram
+            </button>
+          </a>      
+         </p>
+      </header>
+    </div>
+    </div>
+    )
+
+  
+
+}
+
+
